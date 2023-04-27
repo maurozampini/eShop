4290a49d3b73c3dc0ba2b7f3973881c7e15251a9
Mauro Zampini <mauro.zampini@hotmail.com>
2023-04-27 01:04:01 -0300

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/checkout' element={
+            <>
+              <Header />
+              <Checkout />
+            </>
+          } />
+          <Route path='/' element={
+            <>
+              <Header />
+              <Home />
+            </>
+          } />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,18 @@ import React from 'react'
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
     return (
         <div className='header'>
-            <div className='header__logo'>
-                <StorefrontIcon className='header__logoImage' fontSize='large' />
-                <h2 className='header__logoTitle'>eShop</h2>
-            </div>
-
+            <Link to="/" style={{ textDecoration: "none" }}>
+                <div className='header__logo'>
+                    <StorefrontIcon className='header__logoImage' fontSize='large' />
+                    <h2 className='header__logoTitle'>eShop</h2>
+                </div>
+            </Link>
             <div className='header__search'>
                 <input type='text' className='header__searchInput'></input>
                 <SearchIcon className='header_searchIcon' />
@@ -26,10 +28,12 @@ const Header = () => {
                     <span className='nav__itemLineOne'>Your</span>
                     <span className='nav__itemLineTwo'>Shop</span>
                 </div>
-                <div className='nav__itemBasket'>
-                    <ShoppingBasketIcon />
-                    <span className='nav__itemLineTwo nav__basketCount'>0</span>
-                </div>
+                <Link to="/checkout" style={{ textDecoration: "none" }}>
+                    <div className='nav__itemBasket'>
+                        <ShoppingBasketIcon />
+                        <span className='nav__itemLineTwo nav__basketCount'>0</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
